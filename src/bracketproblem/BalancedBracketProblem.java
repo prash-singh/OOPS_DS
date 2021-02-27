21f1924bb8e5b53cffd9d1f793d110c75d318694
Prashant Singh <prashantsingh2k2@gmail.com>
2021-02-27 09:27:18 +0530

    First Commit

---
+++ src/bracketproblem/BalancedBracketProblem.java
@@ -0,0 +1,40 @@
+//(a+b) * (c+d) => true;
+//{[()]} => true;
+//{[}] => false;
+    /*
+        note => ignore all other variable and operator just push and pop brackets
+        If you get another bracket then peek in stack and compare both of them if
+        they matched then pop the bracket from stack and do same for rest all.
+        If stack if not empty print not a balanced bracket else print balanced
+        bracket.
+     */
+package bracketproblem;
+public class BalancedBracketProblem {
+    String str;
+    public BalancedBracketProblem(String str){
+        this.str = str;
+    }
+
+    public void check(){
+        StackOfBracket obj = new StackOfBracket();
+        char array[] = str.toCharArray();
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] == '(' || array[i] == '[' || array[i] == '{'){
+                obj.push(array[i]);
+            }
+            else if(array[i] == ')' || array[i] == ']' || array[i] == '}'){
+                char chr = obj.peek();
+                if(obj.isEmpty()){
+                    System.out.println("invalid");
+                    return;
+                }else if(array[i] == ')' && chr == '('
+                        || array[i] == ']' && chr == '['
+                        || array[i] == '}' && chr == '{'){
+                    obj.pop();
+                }else{
+
+                }
+            }
+        }
+    }
+}
---
+++ src/bracketproblem/BalancedBracketProblemMain.java
@@ -0,0 +1,12 @@
+package bracketproblem;
+
+import java.util.Scanner;
+
+public class BalancedBracketProblemMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter expression");
+        String str = sc.next();
+
+    }
+}
